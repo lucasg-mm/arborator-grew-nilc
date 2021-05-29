ca1628bf4d9e32ad9b48a43485fc86673a243c05
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-05-29 14:09:49 -0300

    Updated prod URL

--- frontend/urls.js
+++ frontend/urls.js
@@ -1,5 +1,5 @@
 // Change these URLs to the ones used by your proxy
 module.exports = {
   devUrl: "https://localhost:8080", // development environment proxy URL
-  prodUrl: "https://fw.nilc.icmc.usp.br:23680", // production environment proxy URL
+  prodUrl: "https://arborator.icmc.usp.br", // production environment proxy URL
 };
