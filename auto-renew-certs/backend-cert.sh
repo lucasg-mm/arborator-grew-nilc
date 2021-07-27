698321834b5d893aa729df33548d1e6399fdface
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-07-27 15:55:33 -0300

    Fixed certificate auto renew

--- auto-renew-certs/backend-cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# gets the path to the root directory of the project
-root_path="$(dirname $(dirname $(realpath $0)))"
-
-# generating self signed certificates for the next year
-openssl req -x509 -newkey rsa:4096 -nodes -out $root_path/backend/keys/cert.pem -keyout $root_path/backend/keys/arborator-grew-dev.pem -days 365 < $root_path/cert_template.txt
-openssl req -x509 -newkey rsa:4096 -nodes -out $root_path/backend/keys/cert.pem -keyout $root_path/backend/keys/arborator-grew.pem -days 365 < $root_path/cert_template.txt
--- auto-renew-certs/frontend-cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# gets path to the docker-compose file
-compose_path="$(dirname $(dirname $(realpath $0)))/docker-compose.yml"
-
-# restarts frontend service to get another ad-hoc
-# certificate
-docker-compose -f $compose_path down frontend
-docker-compose -f $compose_path up frontend
